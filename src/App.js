52b09c8ae034643df2438b24fe6dc584fedbbd5a
Sankhasubhra Roy <sankha.asansol@gmail.com>
2023-05-17 17:48:47 +0530

    Navbar Updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar/Navbar';
+import Menu from './components/Menu/Menu'
 
 function App() {
   return (
-    <Navbar />
+    // <Navbar />
+    <Menu />
   );
 }
 
