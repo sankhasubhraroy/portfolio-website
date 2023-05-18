b464b6e42e260ace7703fcf82931d2664e90bf2d
Sankhasubhra Roy <sankha.asansol@gmail.com>
2023-05-18 17:52:22 +0530

    Added Intro

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import Navbar from './components/Navbar/Navbar';
 import Menu from './components/Menu/Menu'
 import PhotoFrame from './components/PhotoFrame/PhotoFrame';
 import SocialTab from './components/SocialTab/SocialTab';
+import Intro from './components/Intro/Intro';
 
 function App() {
   return (
     // <Navbar />
     // <Menu />
     // <PhotoFrame />
-    <SocialTab />
+    // <SocialTab />
+    <Intro />
   );
 }
 
---
+++ src/components/Intro/Intro.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Intro.css'
+
+export default function Intro() {
+    return (
+        <div className='intro'>
+            <p>Hello,</p>
+            <h1>I'm Sankhasubhra Roy</h1>
+            <h2>Fullstack Developer</h2>
+            <div className="attachment">
+                <a href="#">
+                    <span>Resume</span>
+                </a>
+                <a href="#">
+                    <span>My Work</span>
+                </a>
+            </div>
+        </div>
+    )
+}
