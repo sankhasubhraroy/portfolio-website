c541aa7ba2aeb7243500960893c0a4251bad3575
Sankhasubhra Roy <sankha.asansol@gmail.com>
2023-05-18 14:13:47 +0530

    Added Menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Menu from './components/Menu/Menu'
+import PhotoFrame from './components/PhotoFrame/PhotoFrame';
+import SocialTab from './components/SocialTab/SocialTab';
 
 function App() {
   return (
     // <Navbar />
     <Menu />
+    // <PhotoFrame />
+    // <SocialTab />
   );
 }
 
