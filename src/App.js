3e0d5743fcd88fdf5fe7479d26a01d5ee8f9e08a
Sankhasubhra Roy <sankha.asansol@gmail.com>
2023-05-21 00:59:02 +0530

    Added ProjectCard

--- src/App.js
+++ src/App.js
@@ -6,17 +6,23 @@ import SocialTab from './components/SocialTab/SocialTab';
 import Intro from './components/Intro/Intro';
 import Home from './pages/Home/Home';
 import MenuItem from './components/MenuItem/MenuItem';
+import ProjectCard from './components/ProjectCard/ProjectCard';
 
 function App() {
   return (
     <>
-      <Navbar />
+      {/* <Navbar /> */}
       {/* <MenuItem /> */}
       {/* <PhotoFrame /> */}
       {/* <SocialTab /> */}
       {/* <Intro /> */}
-      <Home />
-      <Menu />
+      {/* <Home /> */}
+      {/* <Menu /> */}
+      <ProjectCard
+        heading='ParkNow'
+        body='ParkNow is a web-based application designed to streamline and automate the process of managing vehicle parking by just one click. It provides a convenient and efficient solution for users, ensuring a smooth parking experience for all.'
+        isVisit={true}
+      />
     </>
   );
 }
